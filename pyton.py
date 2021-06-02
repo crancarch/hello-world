724e3a5bcbbb50628fe80c9c902eb13a8fceccb8
Archit Gupta <43511865+crancarch@users.noreply.github.com>
2021-06-02 11:23:58 +0530

    added python file

---
+++ pyton.py
@@ -0,0 +1,35 @@
+
+file1 = open("myfile.txt","w")
+L = ["Delhi \n","Paris \n","London \n"]
+
+file1.write("Hello \n")
+file1.writelines(L)
+file1.close()
+
+file1 = open("myfile.txt","r+")
+
+print "Output of Read function is "
+print file1.read()
+print
+
+file1.seek(0)
+
+print "Output of Readline function is "
+print file1.readline()
+print
+
+file1.seek(0)
+print "Output of Read(9) function is "
+print file1.read(9)
+print
+
+file1.seek(0)
+
+print "Output of Readline(9) function is "
+print file1.readline(9)
+
+file1.seek(0)
+print "Output of Readlines function is "
+print file1.readlines()
+print
+file1.close()
